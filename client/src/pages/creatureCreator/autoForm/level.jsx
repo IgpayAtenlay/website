0c01d6ec1dc14c8616285d0b35966a113a809c1a
IgpayAtenlay <mscron7@gmail.com>
2024-06-20 18:04:12 -0400

    level updates skills with scale

--- client/src/pages/creatureCreator/autoForm/level.jsx
+++ client/src/pages/creatureCreator/autoForm/level.jsx
@@ -1,13 +1,34 @@
 import {useContext} from 'react';
 import {CreatureContext} from "../index";
+import { skillModifiers } from '../variables';
 
 export default function Level() {
     var {creature, setCreature} = useContext(CreatureContext);
 
     function handleChange(e) {
+
+        // update level
+        var level = parseInt(e.target.value);
+
+        // update skills
+        var skills = creature.skills;
+
+        if (level >= -1 && level <= 24) {
+            skills.forEach((skill, index) => {
+                if (skill.scale !== "auto") {
+                    var value = skillModifiers[skill.scale][level + 1];
+                    skills = skills.with(index, {
+                        ...skills[index],
+                        value: value
+                    })
+                }
+            });
+        }
+
         setCreature(prevCreature => ({
             ...prevCreature,
-            level: parseInt(e.target.value)
+            level: level,
+            skills: skills
         }));
     }
 
