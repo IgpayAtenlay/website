d242eba94db3853e7d99f6abbdaca767b6e17357
IgpayAtenlay <mscron7@gmail.com>
2024-08-22 10:51:53 -0400

    replaced bad table

--- client/src/pages/creatureCreator/autoForm/skills/index.jsx
+++ client/src/pages/creatureCreator/autoForm/skills/index.jsx
@@ -9,33 +9,22 @@ import DeleteButton from './deleteButton';
 export default function Skills() {
     var {creature} = useContext(CreatureContext);
 
-    var skills = creature.skills.map(e => {
-        return (
-            <tr key={e.id}>
-                <th scope='row'><Name id={e.id} name={e.name} /></th>
-                <td><Scale id={e.id} scale={e.scale} /></td>
-                <td><DeleteButton id={e.id} /></td>
-            </tr>
-        );
+    var skills = [];
+    creature.skills.forEach(e => {
+        skills = skills.concat(<Name id={e.id} name={e.name} key={e.id + "name"}/>);
+        skills = skills.concat(<Scale id={e.id} scale={e.scale} key={e.id + "scale"} />);
+        skills = skills.concat(<DeleteButton id={e.id} key={e.id + "delete"} />);
     }
         
     );
 
     return (
         <div>
-            <table>
-                <caption>Skills</caption>
-                <thead>
-                    <tr>
-                        <th scope='col'>Skill</th>
-                        <th scope='col'>Scale</th>
-                        <th scope='col'>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {skills}
-                </tbody>
-            </table>
+            <b>Skills</b>
+            <div className='skillWrapper'>
+                {skills}
+            </div>
+            
             <AddButton />
             <SortButton />
         </div>
